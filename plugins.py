81c49a16e3c575052fcc7e50f949a0e3a5ef9fb4
Taiiwo <tylerzacc@gmail.com>
2013-11-25 00:15:46 +0000

    \!wa now ignores unicode chars and prints multiline

--- plugins.py
+++ plugins.py
@@ -9,7 +9,7 @@
 #  numr = user specified value of how many links to ignore (int)
 #  channel = user specified channel name (str)
 from util import *
-import urllib
+import urllib, json
 from lxml import etree
 class plugins(object):
 	def __init__(self,data):
@@ -231,10 +231,83 @@ class plugins(object):
 			args = argv('!send',data['recv'])
 			if args['user'] in data['admins']:
 				return ' '.join(args['argv'][1:])+ '\r\n'
+
+	def chansearch(self, data):
+	    if '!4search' in data['recv']:
+	        args = argv('!4search',data['recv'])
+	        if args['argv'][1] == 'usage':
+	            return say(args['channel'], '!4search b op/all Taiiwo Trollstrich TJ')
+	        board = args['argv'][1]
+	        arg = args['argv'][2]#Should be either all or op
+            words = args['argv'][3:]
+            #Doing stupid TJ's suggestion:
+            for word in words:
+                for letter in word:
+                    if letter == '-':
+                        letter = ' '
+            results = []
+            rawjson = urllib2.urlopen('http://api.4chan.org/' + board + '/catalog.json').read()
+            time.sleep(1)
+            parsedjson = json.loads(rawjson)
+            count = 0
+            pagecount = 0
+            retme = []
+            for page in parsedjson:
+                print 'Searching page ' + str(count)
+                count += 1
+                threadcount = 0
+                for thread in page['threads']:
+                    if 'args' in locals():
+                        if arg != 'op':
+                            print 'On thread ' + str(threadcount)
+                            threadcount += 1
+                            #get thread number
+                            num = thread['no']
+                            try:
+                                rawreplies = urllib2.urlopen('http://api.4chan.org/' + board + '/res/' + str(num) + '.json').read()
+                            except:
+                                print "Thread 404'd"
+                                break
+                            time.sleep(0.00001)
+                            parsedreplies = json.loads(rawreplies)
+                            for post in parsedreplies['posts']:
+                                if 'com' in post:
+                                    for string in words:
+                                        if string in post['com']: # (Thinking of checking post['name']
+                                            if num == post['no']:
+                                                retme.append('http://boards.4chan.org/' + board + '/res/' + str(num))
+                                                print 'http://boards.4chan.org/' + board + '/res/' + str(num)
+                                            else:
+                                                retme.append('http://boards.4chan.org/' + board + '/res/' + str(num) + '#p' + str(post['no']))
+                                                print 'http://boards.4chan.org/' + board + '/res/' + str(num) + '#p' + str(post['no'])
+                        else:
+                            #print pagecount
+                            pagecount += 1
+                            if 'com' in thread:
+                                for string in words:
+                                    if string in thread['com']:
+                                        retme.append('http://boards.4chan.org/' + board + '/res/' + str(thread['no']))
+                                        print 'http://boards.4chan.org/' + board + '/res/' + str(thread['no'])
+            if retme == []:
+                return say(args['nick'], "No threads found")
+            retmeforrealthistime = []
+            lt = 15
+            appendloop = 0
+            for i in retme:
+                if appendloop <= lt:
+                    retmeforrealthistime.append(say(args['nick'], i))
+                appendloop += 1
+            return ''.join(retmeforrealthistime)
 	def wa(self, data):
 		if '!wa' in data['recv']:
 			args = argv('!wa',data['recv'])
-			response = urllib2.urlopen('http://api.wolframalpha.com/v2/query?input=pi&appid=Y76J7A-WKLWTY7RKJ').read()
+			query = '%20'.join(args['argv'][1:])
+			response = urllib2.urlopen('http://api.wolframalpha.com/v2/query?input=' + query + '&appid=Y76J7A-WKLWTY7RKJ').read()
 			tree = etree.XML(response)
 			answer = tree.findall('pod')[1].find('subpod').find('plaintext').text
-			return say(args['channel'],answer)						
+			answer = answer.split('\n')
+			toret = []
+			for i in answer:
+				toret.append(say(args['channel'],i).encode('ascii', 'ignore'))
+			return say(args['channel'],answer)
+
