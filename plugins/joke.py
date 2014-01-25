a369b0682841e4e761c55afc848f5fb3bbba0dcb
OpenShift guest <tylerzacc@gmail.com>
2014-01-25 06:22:19 -0500

    Fixed joke

--- plugins/joke.py
+++ plugins/joke.py
@@ -1,13 +1,14 @@
 def main(data):
-	if '!joke' in data['recv']:
-		loop = 1
-		args = argv('!joke',data['recv'])
-		while loop == 1:
-			html = urllib2.urlopen('http://www.sickipedia.org/getjokes/random').read()
-			soup = BeautifulSoup(html)
-			joke = soup.body.find('div', attrs={'class':'jokeText'}).text
-			if len(joke) <= 300:
-				loop = 0
-			ujoke = joke.decode("utf-8")
-			joke = html_decode(ujoke.encode("ascii","ignore"))
-			return 'privmsg ' + args['channel'] + ' :' + joke + '\r\n'
+        if '!joke' in data['recv']:
+                args = argv('!joke',data['recv'])
+                while 1:
+                        html = urllib2.urlopen('http://www.sickipedia.org/random').read()
+                        soup = BeautifulSoup(html)
+                        joke = soup.body.find('section', attrs={'class':'jokeText'}).text #div -> section. It works.
+                        #   joke = unicode(joke) 1 problem I've found - jokes are not nearly as funny as they used to be
+                        if len(joke) <= 250:
+                                joke = joke.replace('\n', ' ')
+                                joke = joke.decode("utf-8")
+                                joke = html_decode(joke.encode("ascii","ignore"))
+                                return 'privmsg ' + args['channel'] + ' :' + joke + '\r\n'
+                return 'privmsg ' + args['channel'] + ' :' + joke + '\r\n'
