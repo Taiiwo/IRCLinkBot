afdff56b0cd15ae3bd701d639e24891496fb6025
Taiiwo Llort <tylerzacc@gmail.com>
2014-01-25 00:25:20 +0000

    fix a bug causing it to return an error every time the plugin doesn't run

--- plugins/say.py
+++ plugins/say.py
@@ -1,5 +1,6 @@
 def main(data):
-	args = argv("!say", data['recv'])
-	if args['argv'][0] == '!say' and args['nick'] in self.authnick:
-		say = ' '.join(args['argv'][2:])
-		return 'privmsg ' + args['argv'][1] + ' :' + say + '\r\n'
+	if '!say' in data['recv']:
+		args = argv("!say", data['recv'])
+		if args['nick'] in self.authnick:
+			say = ' '.join(args['argv'][2:])
+			return 'privmsg ' + args['argv'][1] + ' :' + say + '\r\n'
