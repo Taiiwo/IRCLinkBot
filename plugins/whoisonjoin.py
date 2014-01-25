8a8eba3e354db65ec3b1917989c7b7ca17656bf1
OpenShift System User <you@example.com>
2014-01-25 17:30:05 -0500

    Only whois people we know

--- plugins/whoisonjoin.py
+++ plugins/whoisonjoin.py
@@ -2,5 +2,10 @@ def main(data):
         #:Taiiwo!~Taiiwo@cpc3-nott16-2-0-cust414.12-2.cable.virginm.net JOIN ##426699k
         if ' JOIN ' in data['recv']:
                 args = argv('JOIN',data['recv'])
-                return 'WHOIS ' + args['nick'] + '\r\n'
+		regdNick = False
+		for user in data['config']['settings']['userModes']:
+			if args['nick'] == user['nick']:
+				regdNick = True
+		if regdNick:
+                	return 'WHOIS ' + args['nick'] + '\r\n'
 
