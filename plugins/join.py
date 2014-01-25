b7caddf1735b7f9b2602eb26b9287287096b6330
Taiiwo Llort <tylerzacc@gmail.com>
2014-01-25 17:14:10 +0000

    Fix join and leave

--- plugins/join.py
+++ plugins/join.py
@@ -2,7 +2,7 @@ def main(data):
         if '!join' in data['recv']:
                 args = argv('!join',data['recv'])
                 for user in data['config']['settings']['userModes']:
-                        if args['nick'] == user['nick'] and args['isAuth'] == 'True':
+                        if args['nick'] == user['nick'] and modeCheck('a',data):
                                 if 'g' in user['modes'] or args['channel'] == user['channel']:
                                         if 'a' in user['modes']:
                                                 return 'JOIN ' + args['argv'][1] + '\r\n'
--- plugins/leave.py
+++ plugins/leave.py
@@ -2,7 +2,7 @@ def main(data):
         if '!leave' in data['recv']:
                 args = argv('!leave',data['recv'])
 		for user in data['config']['settings']['userModes']:
-	                if args['nick'] == user['nick'] and args['isAuth'] == 'True':
+	                if args['nick'] == user['nick'] and modeCheck('a',data):
 				if 'g' in user['modes'] or args['channel'] == user['channel']:
 					if 'a' in user['modes']:
 		                        	return 'PART ' + args['argv'][1] + '\r\n'
