3bbe662a2a4433591b9b1fe747f267845a208ff3
Taiiwo Llort <tylerzacc@gmail.com>
2014-01-25 00:24:30 +0000

    remove lxml, it is no longer a dependency

--- plugins/privmsg/wa.py
+++ plugins/privmsg/wa.py
@@ -1,11 +1,11 @@
-from lxml import etree
+from BeautifulSoup import BeautifulSoup
 def main(data):
 	if '!wa ' in data['recv']:
 		args = argv('!wa',data['recv'])
 		query = '%20'.join(args['argv'][1:])
 		response = urllib2.urlopen('http://api.wolframalpha.com/v2/query?input=' + query + '&appid=Y76J7A-WKLWTY7RKJ').read()
-		tree = etree.XML(response)
-		answer = tree.findall('pod')[1].find('subpod').find('plaintext').text
+		soup = BeautifulSoup(response)
+		answer = soup.queryresult.findAll('pod')[1].subpod.plaintext.text
 		answer = answer.split('\n')
 		toret = []
 		for i in answer:
