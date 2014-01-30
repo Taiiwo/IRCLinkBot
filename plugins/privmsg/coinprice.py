e871b96d6893a20ebddfb24e97aa566cd6067c66
Taiiwo Llort <tylerzacc@gmail.com>
2014-01-30 15:01:42 +0000

    add timezone to config

--- plugins/privmsg/coinprice.py
+++ plugins/privmsg/coinprice.py
@@ -18,8 +18,13 @@ def main(data):
 			pass
 		if prices:# should never be false.
 			ticker = prices['ticker']
-			time = datetime.fromtimestamp(int(ticker['updated'] - 14400)).strftime('%Y-%m-%d %H:%M:%S')
+			timeDiff = data['config']['settings']['hoursDiffGMT']
+			if timeDiff[0] == '-':
+				timeDiff = abs(int(timeDiff[1:])*60*60)
+			elif timeDiff[0] == '+':
+				timeDiff = int(timeDiff[1:])*60*60
+			time = datetime.fromtimestamp(int(ticker['updated'] - timeDiff)).strftime('%Y-%m-%d %H:%M:%S')
 			delay = int(ticker['server_time']) - int(ticker['updated'])
-			delay = datetime.fromtimestamp(delay - 14400).strftime('%H:%M:%S')
+			delay = datetime.fromtimestamp(delay - timeDiff + 3600).strftime('%H:%M:%S')
 			r = "High: %s%s Low: %s%s Last Trans: %s%s Time: %s Delay: %s"%(ticker['high'],cur2,ticker['low'],cur2,ticker['last'],cur2,time,delay)
 			return say(args['channel'],r)
