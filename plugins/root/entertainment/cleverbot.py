75b993de21bf168969e1bfcd2612a82dfb9460b7
Taiiwo Llort <tylerzacc@gmail.com>
2014-02-03 19:37:31 +0000

    missed bracket

--- plugins/root/entertainment/cleverbot.py
+++ plugins/root/entertainment/cleverbot.py
@@ -6,7 +6,7 @@ def main(data):
 		bot = botfac.create_session()
 		args = argv(':', data['recv'])
 		answer = bot.think( ' '.join(args['argv'][1:].replace('CleverBot',data['config']['settings']['botNick']))
-		answer = answer.replace('CleverBot',data['config']['settings']['botNick']
+		answer = answer.replace('CleverBot',data['config']['settings']['botNick'])
 		return say(args['channel'],answer)
 
 		
