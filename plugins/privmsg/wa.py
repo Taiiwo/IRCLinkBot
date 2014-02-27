81046f49a0547b9364c9464695cfec2e54c37157
Taiiwo Llort <tylerzacc@gmail.com>
2014-02-26 20:33:02 -0500

    Add failure message to IRC

--- plugins/privmsg/wa.py
+++ plugins/privmsg/wa.py
@@ -5,9 +5,12 @@ def main(data):
 		query = '%20'.join(args['argv'][1:])
 		response = urllib2.urlopen('http://api.wolframalpha.com/v2/query?input=' + query + '&appid=QPEPAR-TKWEJ3W7VA').read()
 		soup = BeautifulSoup(response)
-		answer = soup.queryresult.findAll('pod')[1].subpod.plaintext.string
-		answer = answer.split('\n')
-		toret = []
-		for i in answer:
-			toret.append(say(args['channel'],i).encode('ascii', 'ignore'))
-		return ''.join(toret)
+		try:
+			answer = soup.queryresult.findAll('pod')[1].subpod.plaintext.string
+			answer = answer.split('\n')
+			toret = []
+			for i in answer:
+				toret.append(say(args['channel'],i).encode('ascii', 'ignore'))
+			return ''.join(toret)
+		except:
+			return say(args['channel'], "Information not available.")
