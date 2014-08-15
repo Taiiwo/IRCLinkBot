f3fb3cb462e7f273a09b474a98b2fb689454cfc3
Taiiwo Llort <tylerzacc@gmail.com>
2014-08-15 22:14:51 +0100

    Made it so that TaiiwoBot can join channels that he has in his join list, but is not in them for whatever reason

--- plugins/root/join.py
+++ plugins/root/join.py
@@ -4,9 +4,9 @@ def main(data):
                 for user in data['config']['settings']['userModes']:
                         if args['nick'] == user['nick'] and modeCheck('a',data):
                                 if 'g' in user['modes'] or args['channel'] == user['channel']:
-                                        if 'a' in user['modes'] and args['argv'][1][0] == '\#':
+                                        if args['argv'][1][0] == "#":
 						#if channel not in channel list
-						if not args['argv'][1].lower() in data['joinChannels']:
+						if not args['argv'][1].lower() in data['config']['settings']['joinChannels']:
 							#add it
                                                 	data['config']['settings']['joinChannels'].append(args['argv'][1].lower())
                                                 	saveConfigChanges(data['config'])
