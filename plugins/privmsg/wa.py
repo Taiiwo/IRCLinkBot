e26c124a7433966dc64973a105c5ed8316bb8070
Your Name <you@example.com>
2014-12-02 14:00:39 -0500

    urlencode query properly

--- plugins/privmsg/wa.py
+++ plugins/privmsg/wa.py
@@ -1,9 +1,12 @@
 from BeautifulSoup import BeautifulSoup
 def main(data):
 	if '!wa ' in data['recv']:
+		import urllib
 		args = argv('!wa',data['recv'])
-		query = '%20'.join(args['argv'][1:])
-		response = urllib2.urlopen('http://api.wolframalpha.com/v2/query?input=' + query + '&appid=QPEPAR-TKWEJ3W7VA').read()
+		query = ' '.join(args['argv'][1:])
+		query = {'input': query, 'appid': 'QPEPAR-TKWEJ3W7VA'}
+		query = urllib.urlencode(query)
+		response = urllib2.urlopen('http://api.wolframalpha.com/v2/query?' + query).read()
 		soup = BeautifulSoup(response)
 		try:
 			interp = soup.queryresult.findAll('pod')[0].subpod.plaintext.string
@@ -14,4 +17,16 @@ def main(data):
 				toret.append(say(args['channel'],i).encode('ascii', 'ignore'))
 			return ''.join(toret)
 		except:
-			return say(args['channel'], "Information not available.")
+			global bot
+			query = ' '.join(args['argv'][1:])
+			query = query.replace('\n','')
+	                query = query.replace('\r','')
+	                query = query.replace(data['config']['settings']['botNick'] + ':','')
+	                query = query.replace(data['config']['settings']['botNick'],'CleverBot')
+	                answer = bot.think(query)
+	                answer = answer.replace('CleverBot',data['config']['settings']['botNick'])
+	                answer = answer.replace('Cleverbot',data['config']['settings']['botNick'])
+	                answer = answer.replace('God','Taiiwo')
+	                answer = answer.replace('god','Taiiwo')
+	                answer = answer.replace('&ouml;', 'o')
+			return say(args['channel'], args['nick'] + ": " + answer)
