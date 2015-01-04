a0f506a5037982a8a3f4708d0fd7049e73e984b3
Your Name <you@example.com>
2015-01-04 18:47:14 -0500

    Plugin that scrapes links posted and archives thier contents

---
+++ plugins/privmsg/liblinkScraper.py
@@ -0,0 +1,22 @@
+def main(data):
+	url = geturl(data['recv'])
+	if url:# if there's a url in something someone says
+		timestamp = time.now()
+		if re.match("^https?://infotomb\.com/.*$", url):
+			url = # code to download link
+		if re.match("^https?://pomf\.se/.*$", url):
+			url = # code to download link
+		# download file
+		contents = urllib2.urlopen(url).read()
+		file = open('./archive/' + timestamp + fileExtension, 'rw')
+		file.write(contents)
+		# log data on file to a log
+		args = argv(data['recv'])
+		log = {}
+		log['url'] 		= url
+		log['timestamp'] 	= timestamp
+		log['poster'] 		= args['nick']
+		log['fileLocation'] 	= "./archive/" + timestamp + fileExtension# need regex for this
+		log['md5']		= hashlib.md5(contents.encode('utf-8')).hexdigest())
+		logFile = open('archive.json', 'rw')
+		logFile.write(json.dumps(json.loads(logFile.read()).append(log)))
