811c7270ff9cf039cd05b02a4691b41f27706265
Taiiwo <tylerzacc@gmail.com>
2015-01-05 13:38:27 +0000

    Update liblinkScraper.py

--- plugins/privmsg/liblinkScraper.py
+++ plugins/privmsg/liblinkScraper.py
@@ -3,6 +3,29 @@ def main(data):
 	if url:# if there's a url in something someone says
 		timestamp = time.now()
 		if re.match("^https?://infotomb\.com/.*$", url):
+			paste_info = {
+					'site': 'infotomb',
+					'url': url,
+				}
+			response = requests.get(url)
+			if response.status_code != 200:
+				return
+			m = re.match('^.*/([0-9a-zA-Z]+)(\.([a-zA-Z]+))?$',url)
+			if not m.group(2):
+				data = response.text
+				soup = BeautifulSoup(data)
+				url = soup.find_all('input')[2]['value']
+				m = re.match('^.*/([0-9a-zA-Z]+)(\.([a-zA-Z]+))?$',url)
+				if m:
+					response = requests.get(url)
+					if response.status_code != 200:
+					return
+				else:
+					return 
+				idd = m.group(3)
+				paste_info['content'] = response.content
+				paste_info['orig_filename'] = m.group(1)
+				paste_info['extension'] = idd
 			url = # code to download link
 		if re.match("^https?://pomf\.se/.*$", url):
 			url = # code to download link
