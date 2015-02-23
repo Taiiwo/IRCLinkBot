eb4ff0b2b455549b5597b6ef4c4129a75f5be5db
Taiiwo Llort <tylerzacc@gmail.com>
2015-02-23 22:16:54 +0000

    Use the new API to send to the target

--- plugins/root/say.py
+++ plugins/root/say.py
@@ -1,6 +1,6 @@
 def main(data):
-	if '!say' in data['recv']:
-		args = argv("!say", data['recv'])
-		if modeCheck('a', data):
-			say = ' '.join(args['argv'][2:])
-			return 'privmsg ' + args['argv'][1] + ' :' + say + '\r\n'
+    if '!say' in data['recv']:
+        args = argv("!say", data['recv'])
+        if modeCheck('a', data):
+            message = ' '.join(args['argv'][2:])
+            data['api'].say(args['argv'][1], message)
