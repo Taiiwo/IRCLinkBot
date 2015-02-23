cb45918d0804d918988ddd343637a726b308bb04
Taiiwo Llort <tylerzacc@gmail.com>
2015-02-23 22:23:46 +0000

    prettify

--- plugins/privmsg/cleverbot.py
+++ plugins/privmsg/cleverbot.py
@@ -1,27 +1,27 @@
 def main(data):
-	if data['config']['settings']['botNick'] in data['recv'] or data['config']['settings']['botNick'].lower() in data['recv']:
-		from libchatterbot import ChatterBotFactory, ChatterBotType
-		global cleverbot
-		if not 'cleverbot' in globals():# check for instance
-			factory = ChatterBotFactory()
-			botfac = factory.create(ChatterBotType.CLEVERBOT)#JABBERWACKY
-			cleverbot = botfac.create_session()
-			print "making new bot"
-		args = argv('@', data['recv'])
-		junk = ':' + textbetween(':',' :', data['recv']) + ' :'
-		query = data['recv'].replace(junk, '')
-		query = query.replace('\n','')
-		query = query.replace('\r','')
-		query = query.replace(data['config']['settings']['botNick'] + ':','')
-		query = query.replace(data['config']['settings']['botNick'],'CleverBot')
-		answer = cleverbot.think(query)
-		answer = answer.replace('CleverBot',data['config']['settings']['botNick'])
-		answer = answer.replace('Cleverbot',data['config']['settings']['botNick'])
-		answer = answer.replace('God','Taiiwo')
-		answer = answer.replace('god','Taiiwo')
-		answer = answer.replace('&ouml;', 'o')
-		debug = 'Query: ' + query + ' -- Answer: "' + answer + '"'
-		print debug
-		
-		return say(args['channel'],args['nick'] + ': ' + answer)
-		
+    if data['config']['settings']['botNick'] in data['recv']\
+            or data['config']['settings']['botNick'].lower() in data['recv']:
+        from libchatterbot import ChatterBotFactory, ChatterBotType
+        global cleverbot
+        if not 'cleverbot' in globals():  # check for instance
+            factory = ChatterBotFactory()
+            botfac = factory.create(ChatterBotType.CLEVERBOT)  # JABBERWACKY
+            cleverbot = botfac.create_session()
+            # print "making new bot"
+        args = argv('', data['recv'])
+        query = args['message']
+        query = query.replace('\n','')
+        query = query.replace('\r','')
+        query = query.replace(data['config']['settings']['botNick'] + ':','')
+        query = query.replace(data['config']['settings']['botNick'],'CleverBot')
+        query = query.encode('utf-8')
+        answer = html_decode(cleverbot.think(query))
+        answer = answer.replace('CleverBot',data['config']['settings']['botNick'])
+        answer = answer.replace('Cleverbot',data['config']['settings']['botNick'])
+        answer = answer.replace('God','Taiiwo')
+        answer = answer.replace('god','Taiiwo')
+        debug = 'Query: ' + query.decode('utf-8') + ' -- Answer: "' + answer + '"'
+        print debug
+        
+        data['api'].say(args['channel'], args['nick'] + ': ' + answer)
+        
