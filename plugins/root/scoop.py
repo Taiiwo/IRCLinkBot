045c1c1400ac386b46f36f9613b3f46b2476ab1b
Taiiwo Llort <tylerzacc@gmail.com>
2016-01-11 10:55:41 -0500

    update to work with new api installation

--- plugins/root/scoop.py
+++ plugins/root/scoop.py
@@ -11,16 +11,26 @@ def main(data):
 			else:
 				queryStatus = True
 			if queryStatus == True:
-				try:
-					ratio = int(args['argv'][3])
-				except:
+                                def sumLink(url, ratio):
+				    scoop = urllib2.urlopen("http://phantas.ml/ots/?to_sum="+ url +"&ratio=" + str(ratio)).read()
+				    scoop = html_decode(scoop)
+				    scoop = html_strip(scoop)
+				    scoop = " ".join(scoop.split())
+                                    return scoop
+				if len(args['argv']) == 4 and args['argv'][3].isdigit():
+					ratio = args['argv'][3]
+				else:
 					ratio = 10
 				url = args['argv'][2]
-				scoop = urllib2.urlopen("http://thescoop.io/ots.php?to_sum="+ url +"&ratio=" + str(ratio)).read()
-				scoop = html_decode(scoop)
-				scoop = html_strip(scoop)
-				scoop = " ".join(scoop.split())
 				if args['argv'][1] == 'me':
+                                        scoop = sumLink(url, ratio)
 					return say(args['nick'],scoop)
 				else:
+                                        if args['argv'][1][0] == "#":
+                                            while 1:
+                                                scoop = sumLink(url, ratio)
+                                                if len(scoop) < 280:
+                                                    break
+                                                else:
+                                                    ratio -= 5
 					return say(args['argv'][1], scoop)
