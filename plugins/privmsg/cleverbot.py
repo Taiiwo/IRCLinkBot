b9bd86d4ec3ca4bbb366caa2f1744fafbf102e76
Taiiwo Llort <tylerzacc@gmail.com>
2016-06-20 10:42:12 -0400

    Use pip's cleverbot instead of old lib

--- plugins/privmsg/cleverbot.py
+++ plugins/privmsg/cleverbot.py
@@ -1,12 +1,10 @@
 def main(data):
     if data['config']['settings']['botNick'] in data['recv']\
             or data['config']['settings']['botNick'].lower() in data['recv']:
-        from libchatterbot import ChatterBotFactory, ChatterBotType
+        from cleverbot import Cleverbot
         global cleverbot
         if not 'cleverbot' in globals():  # check for instance
-            factory = ChatterBotFactory()
-            botfac = factory.create(ChatterBotType.CLEVERBOT)  # JABBERWACKY
-            cleverbot = botfac.create_session()
+            cleverbot = Cleverbot()
             # print "making new bot"
         args = argv('', data['recv'])
         query = args['message']
@@ -14,7 +12,7 @@ def main(data):
         query = query.replace('\r','')
         query = query.replace(data['config']['settings']['botNick'] + ':','')
         query = query.replace(data['config']['settings']['botNick'],'CleverBot')
-        answer = html_decode(cleverbot.think(query))
+        answer = html_decode(cleverbot.ask(query))
         answer = answer.replace('CleverBot',data['config']['settings']['botNick'])
         answer = answer.replace('Cleverbot',data['config']['settings']['botNick'])
         answer = answer.replace('God','Taiiwo')
