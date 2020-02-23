0fba9f5326ffe0b306a1206c19ef98b2e1c9701b
Taiiwo Llort <Taiiwo@Llort.gq>
2020-02-23 13:59:48 +0000

    Implement a base plugin class

--- taiiwobot/plugin.py
+++ taiiwobot/plugin.py
@@ -1,2 +1,11 @@
-class PluginBase:
-    pass
+class Plugin:
+    def unload(self):
+        if hasattr(self, "interface"):
+            self._unloaded = True
+            del self.interface
+
+    def mention(self, user):
+        return user
+
+    def code_block(self, text):
+        return text
