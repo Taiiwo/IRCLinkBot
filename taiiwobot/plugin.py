34eea96f0f3b4fdebbef8f80dcda0435eef21965
Taiiwo Llort <Taiiwo@Llort.gq>
2020-03-05 22:21:27 -0500

    Some base plugin methods

--- taiiwobot/plugin.py
+++ taiiwobot/plugin.py
@@ -3,3 +3,9 @@ class Plugin:
         if hasattr(self, "interface"):
             self._unloaded = True
             del self.interface
+
+    def mention(self, user):
+        return user
+
+    def code_block(self, text):
+        return text
