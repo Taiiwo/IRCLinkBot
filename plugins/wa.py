97343936dcbc6a4cf9814a4dbdf3f6f06abea0c8
Taiiwo Llort <Taiiwo@Llort.gq>
2020-03-05 22:19:29 -0500

    Change class to WA, respond on error, and general formatting

--- plugins/wa.py
+++ plugins/wa.py
@@ -5,25 +5,24 @@ import html
 import re
 from taiiwobot.plugin import Plugin
 
-class WolframAlpha(Plugin):
+
+class WA(Plugin):
     def __init__(self, bot):
         self.bot = bot
         self.interface = bot.util.Interface(
             "wa",
-            "Searches wolfram alpha",
-            [
-                "q quiet Does the action quietly 0"
-            ],
-            self.wa
+            "Searches wolfram alpha. Args: <search_query>",
+            ["q quiet responds with only one line of output 0"],
+            self.wa,
         ).listen()
 
     def wa(self, message, *args, quiet=False):
         # URL escape query
-        query = {'input': " ".join(args), 'appid': 'QPEPAR-TKWEJ3W7VA'}
-        baseUrl = 'http://api.wolframalpha.com/v2/query?'
+        query = {"input": " ".join(args), "appid": "QPEPAR-TKWEJ3W7VA"}
+        baseUrl = "http://api.wolframalpha.com/v2/query?"
         response = requests.get(baseUrl, params=query).text
         soup = BeautifulSoup(response, "html.parser")
-        pods = soup.queryresult.findAll('pod')
+        pods = soup.queryresult.findAll("pod")
         if pods and len(pods) >= 2:  # if we got an answer
             answers = []
             # Iterate the results
@@ -38,15 +37,14 @@ class WolframAlpha(Plugin):
                     # Replace it with its corresponding Unicode character
                     try:
                         answer = answer.replace(
-                            match.group(0),
-                            unichr(int(match.group(1), 16))
+                            match.group(0), unichr(int(match.group(1), 16))
                         )
                     except:
                         pass
                 answers.append(answer)
             # Prepare these answers for IRC
             ircAnswersString = ""
-            lines = '\n'.join(answers).splitlines()
+            lines = "\n".join(answers).splitlines()
             postShortLink = False
             if len(lines) > 5:
                 lines = lines[:5]
@@ -56,17 +54,23 @@ class WolframAlpha(Plugin):
                 postShortLink = False
             for line in lines:
                 if len(line) > 430:
-                    ircAnswersString += "".join(line[:427]) + '...\n'
+                    ircAnswersString += "".join(line[:427]) + "...\n"
                 else:
-                    ircAnswersString += line + '\n'
+                    ircAnswersString += line + "\n"
             self.bot.msg(message.target, self.bot.server.code_block(ircAnswersString))
             if postShortLink:
-                shortLink = self.bot.util.maketiny("http://www.wolframalpha.com/input/?" + urlencode({"i": " ".join(args)}))
+                shortLink = self.bot.util.maketiny(
+                    "http://www.wolframalpha.com/input/?"
+                    + urlencode({"i": " ".join(args)})
+                )
                 self.bot.msg(message.target, "More: " + shortLink)
         else:
+            """
             # We didn't get an answer, make cleverbot reply instead
             # Construct the string that makes the cleverbot plugin respond
             cleverBotFlag = self.bot.server.name + ": "
             # emulate how cleverbot would have been run if invoked normally
             message.content = message.content.replace("$wa ", cleverBotFlag)
             self.bot.server.trigger("message", message)
+            """
+            self.bot.msg(message.target, "No data available.")
